a04b96c7607f3d517db477b58d35b35b692198a7
extracteddeath <92632906+extracteddeath@users.noreply.github.com>
2026-07-03 16:30:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    serves navigations from the cached shell. A registered SW with a fetch handler
    is what makes the app installable ("Install app") on Android/Chrome. */
 
-const CACHE = "wpb-shell-v238-0f94d91e-a4b97309";
+const CACHE = "wpb-shell-v239-3e564306-a4b97309";
 const SHELL = [
   "./",
   "./index.html",
